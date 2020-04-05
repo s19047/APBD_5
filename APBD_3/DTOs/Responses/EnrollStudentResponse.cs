4b5ced615567dc108012cb6d88891627dd33c710
Ahmad <s19047@pjwstk.edu.pl>
2020-04-05 23:33:40 +0200

    APBD_5 First Commit

---
+++ APBD_3/DTOs/Responses/EnrollStudentResponse.cs
@@ -0,0 +1,15 @@
+﻿using APBD_3.DTOs.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APBD_3.DTOs.Responses
+{
+    public class EnrollStudentResponse
+    {
+        public EnrollStudentRequest request {get; set;}
+
+        public int semester { get; set; }
+    }
+}
---
+++ APBD_3/Services/IStudentServiceDb.cs
@@ -0,0 +1,21 @@
+﻿using APBD_3.DTOs.Requests;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APBD_3.Services
+{
+    interface IStudentServiceDb
+    {
+        public  void EnrollStudent(EnrollStudentRequest request)
+        {
+            
+        }
+         public  void EnrollStudent2(EnrollStudentRequest request)
+        {
+
+        }
+    }
+}
